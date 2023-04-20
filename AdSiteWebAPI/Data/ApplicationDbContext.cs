1b4deb2dabe640fe6cf3ba088bbee022870aca4d
Frederick <frederick.wennborg@gmail.com>
2023-04-20 16:55:53 +0200

    added User and Advert dbsets

---
+++ AdSiteWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdSiteWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+
+}
---
+++ AdSiteWebAPI/Models/Advert.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdSiteWebAPI.Models
+{
+    public class Advert
+    {
+        [Key]
+        public int AdvertId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int StartingPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+    }
+}

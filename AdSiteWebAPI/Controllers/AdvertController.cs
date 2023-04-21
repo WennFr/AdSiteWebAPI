c3b3c2305008aeadfd009cba16399ab4a7649655
Frederick <frederick.wennborg@gmail.com>
2023-04-21 12:00:29 +0200

    added get all adverts request

--- AdSiteWebAPI/Controllers/AdvertController.cs
+++ AdSiteWebAPI/Controllers/AdvertController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AdSiteWebAPI.Data;
+using AdSiteWebAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdSiteWebAPI.Controllers
 {
@@ -7,5 +10,21 @@ namespace AdSiteWebAPI.Controllers
     [ApiController]
     public class AdvertController : ControllerBase
     {
+        public AdvertController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        private readonly ApplicationDbContext _dbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Advert>>> GetAll()
+        {
+            return Ok(await _dbContext.Adverts.ToListAsync());
+        }
+
+
+
+
     }
 }

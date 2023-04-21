f2daacd6ccc5124ed1e2da5aa26d4d0d1a8073b0
Frederick <frederick.wennborg@gmail.com>
2023-04-21 11:37:15 +0200

    created empty advert-controller

---
+++ AdSiteWebAPI/Controllers/AdvertController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdSiteWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+    }
+}
